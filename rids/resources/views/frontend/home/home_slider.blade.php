736f37e0f5bc02d399bcc6282bae02b572eb7935
aaflores7 <aaflores7@up.edu.ph>
2024-11-23 11:48:57 +0800

    Add frontend assets for user side and update routes

---
+++ rids/resources/views/frontend/home/home_slider.blade.php
@@ -0,0 +1,37 @@
+<section class="home-slider position-relative mb-30">
+            <div class="container">
+                <div class="home-slide-cover mt-30">
+                    <div class="hero-slider-1 style-4 dot-style-1 dot-style-1-position-1">
+                        <div class="single-hero-slider single-animation-wrap" style="background-image: url({{asset('frontend/assets/imgs/slider/1.png')}})">
+                            <div class="slider-content">
+                                <h1 class="display-2 mb-40">
+                                    Welcome to<br />
+                                    UP RIDS
+                                </h1>
+                                <p>Reservation, Inventory, and Disposal System</p>
+                                <form class="form-subcriber d-flex">
+                                    <input type="email" placeholder="Your emaill address" />
+                                    <button class="btn" type="submit">Subscribe</button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="single-hero-slider single-animation-wrap" style="background-image: url({{asset('frontend/assets/imgs/slider/2.png')}})">
+                            <div class="slider-content">
+                                <h1 class="display-2 mb-40">
+                                    Borrow Equipments<br />
+                                    you need
+                                </h1>
+                                <p>Save up to 50% off on your first order</p>
+                                <form class="form-subcriber d-flex">
+                                    <input type="email" placeholder="Your email address" />
+                                    <button class="btn" type="submit">Subscribe</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slider-arrow hero-slider-1-arrow"></div>
+                </div>
+            </div>
+        </section>
+
+<!--End hero slider-->
